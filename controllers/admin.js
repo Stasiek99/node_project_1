36356b807b2adf10240211b7b0075722ccdc189c
Stasiek99 <stasiekpilich@gmail.com>
2026-08-29 13:25:01 +0200

    feat(admin): convert product deletion to async DELETE request
      Replaces the delete form's full-page POST with a fetch-based DELETE
      call to /admin/product/:productId, removing the product card from the
      DOM on success instead of reloading the page.

--- controllers/admin.js
+++ controllers/admin.js
@@ -181,9 +181,10 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
-  Product.findById(prodId).then(product => {
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+      .then(product => {
       if (!product) {
           return next(new Error('Could not find product.'));
       }
@@ -192,11 +193,9 @@ exports.postDeleteProduct = (req, res, next) => {
   })
       .then(() => {
           console.log('DESTROYED PRODUCT');
-          res.redirect('/admin/products');
+          res.status(200).json({ message: "Success!" });
       })
       .catch(err => {
-          const error = new Error(err);
-          error.httpStatusCode = 500;
-          return next(error);
+          res.status(500).json({ message: "Deleting product failed." });
       });
 };
